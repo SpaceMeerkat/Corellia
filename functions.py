968c5810b928f817ea9e08462b5dcac73c390247
James Dawson <SpaceMeerkat@github.com>
2019-11-14 15:08:46 +0000

    CORONA files added

--- functions.py
+++ functions.py
@@ -8,9 +8,14 @@ Created on Thu Oct 10 15:59:24 2019
 
 #_____________________________________________________________________________#
 #_____________________________________________________________________________#
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import torch
 
 from cube_generator import cube_generator
-import numpy as np
 
 #_____________________________________________________________________________#
 #_____________________________________________________________________________#
@@ -29,12 +34,51 @@ def return_cube(i):
     """ MULTIPROCESS CREATION OF CUBES WITH VARIOUS POSITION ANGLES AND
         INCLINATIONS """
         
-    pos_ang = np.random.uniform(0,360,1)
-    inc_ang = np.random.uniform(0,90,1)
-    scale_frac = np.random.uniform(0.1,0.4,1)
-    cube = cube_generator(scale_length_frac=scale_frac,pos_ang=pos_ang[0],
-                          inc_ang=inc_ang[0],resolution=1000).cube_creation()
-    return cube,(pos_ang,inc_ang,scale_frac)
+    pos_ang = random.uniform(0,360)
+    b_over_a = random.uniform(0,1)
+    inc_ang = np.rad2deg(np.sqrt(1-(b_over_a)**2))
+    #inc_ang = random.uniform(5,90)
+    scale_frac = random.uniform(0.1,0.4)
+    ah = random.uniform(0.1,1)
+    Vh = random.uniform(50,500)
+    cube = cube_generator(scale_length_frac=scale_frac,pos_ang=pos_ang,
+                          inc_ang=inc_ang,resolution=1000,ah=ah,Vh=Vh).cube_creation()
+       
+    cube/=cube.max()
+    pos_ang = np.deg2rad(pos_ang)
+        
+    return cube,np.cos(pos_ang),np.sin(pos_ang),inc_ang,scale_frac,Vh/500.
 
 #_____________________________________________________________________________#
 #_____________________________________________________________________________#
+    
+def pos_loss(output, target):
+    
+    """ LOSS FUNCTION FOR POSITION ANGLE BASED ON MINIMUM ARC SEPARATION """
+        
+    loss = torch.mean(torch.stack([ (output-target)**2,
+                                   (1-torch.abs(output-target))**2] ).min(dim=0)[0]) 
+    
+    return loss
+
+#_____________________________________________________________________________#
+#_____________________________________________________________________________#
+    
+def recover_pos(theta1,theta2):
+    """ RECOVERING THE ANGLE AFTER A 2D ROTATION USING EULER MATRICES """
+    angle = np.rad2deg(np.arctan2(theta1,theta2)) + 180
+    return angle
+
+#_____________________________________________________________________________#
+#_____________________________________________________________________________#
+
+
+
+
+
+
+
+
+
+
+
---
+++ job.sh
@@ -0,0 +1,6 @@
+for value in `seq 10`
+do
+	python3 ./tester.py
+	echo $value
+done	
+echo All done
